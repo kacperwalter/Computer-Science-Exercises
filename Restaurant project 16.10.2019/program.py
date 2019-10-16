2b67a650badf618ebe09670b00a2bea2fe0ef6e7
Kacper <kacwal@poczta.fm>
2019-10-16 21:25:21 +0200

    added string repr

--- Restaurant project 16.10.2019/program.py
+++ Restaurant project 16.10.2019/program.py
@@ -5,12 +5,15 @@ class Menu:
         self.start_time = start_time
         self.end_time = end_time
 
+    """string representation"""
+    def __repr__(self):
+        return "{} menu is avalible from {} to {}".format(self.name, self.start_time, self.end_time)
 
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }, 11, 16)
 
-early_bird = Menu("early_bird", {
+early_bird = Menu("early bird", {
   'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
 }, 15, 18)
 
@@ -21,4 +24,7 @@ dinner = Menu("dinner", {
 kids = Menu("kids", {
   'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
 }, 11, 21)
+
+"""this will print string representation method"""
+# print(early_bird)
 
