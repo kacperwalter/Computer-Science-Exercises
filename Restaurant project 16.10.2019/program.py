2ce7a789e8d222b86667b1cbc99f40a5b6032181
Kacper <kacwal@poczta.fm>
2019-10-20 15:34:19 +0200

    project done

--- Restaurant project 16.10.2019/program.py
+++ Restaurant project 16.10.2019/program.py
@@ -35,26 +35,36 @@ class Franchise:
 
         return available_menu
 
+
+class Business:
+    def __init__(self, name, franchises):
+        self.name = name
+        self.franchises = franchises
+
+
 brunch_menu = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50,}
 early_bird_menu = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}
 dinner_menu = {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}
 kids_menu = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}
+arepas_menu = {'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50}
 
 brunch = Menu("brunch", brunch_menu, 11, 16)
 early_bird = Menu("early bird", early_bird_menu, 15, 18)
 dinner = Menu("dinner", dinner_menu, 17, 23)
 kids = Menu("kids", kids_menu, 11, 21)
+arepas = Menu('arepas', arepas_menu, 10, 20)
 
-# print(brunch.items)
 """this will print string representation method"""
 # print(early_bird)
 
 """tests the calculate_bill method"""
 # print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
 
-
 """ Creating the restaurant franchises"""
 flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
 new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
+arepas_place = Franchise("189 Fitzgerald Avenue", arepas)
 
-print(flagship_store.available_menus(23))
+"""creating two different buisnesses"""
+business = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+business2 = Business("Take a' Arepa", arepas_place)
