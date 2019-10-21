04495865cbf40c44020ab68f05dfe2ca585073fb
Kacper <kacwal@poczta.fm>
2019-10-21 19:09:43 +0200

    made some methods

--- Art Marketplace project 20.10.2019/main.py
+++ Art Marketplace project 20.10.2019/main.py
@@ -16,8 +16,18 @@ class Marketplace:
         self.listings = []
 
     def add_listing(self, new_listing):
-        pass
+        self.listings.append(new_listing)
+
+    def remove_listing(self, listing):
+        self.listings.remove(listing)
+
+    def show_listings(self):
+        for listing in self.listings:
+            print(listing)
 
 
 girl_with_mandolin = Art("Picasso, Pablo", '"Girl with a Mandolin (Fanny Tellier)"', "Oil on canvas", 1910)
 print(girl_with_mandolin)
+
+vaneer = Marketplace()
+print(vaneer.show_listings())
