6796bc44c8b4343dd98cd324a58741b27aba73ea
Kacper <kacwal@poczta.fm>
2019-10-29 19:31:21 +0100

    next exercise done

--- Data Structures 29.10.2019/implementing stacks.py
+++ Data Structures 29.10.2019/implementing stacks.py
@@ -24,14 +24,25 @@ Set the stack instance’s top_item equal to the new item, adding it to the top 
 Create a variable item_to_remove and set it equal to the stack’s top_item
 If we’re removing our stack’s top_item, we need to set a new top_item! Set the top_item equal to the node after item_to_remove
 Return the value stored in item_to_remove
+
+3. In __init__(), let’s add two new properties: size and limit.
+
+limit should be accepted as a parameter with a default of 1000. Inside the method, set the instance limit property to the passed in value of limit.
+
+size should be set to 0 in __init__().
+
+4. In peek() and pop(), wrap the current body of each method in an if clause that checks if the size of the stack is greater than 0.
+
+At the end of each method, outside the if clause, add an else clause with a print statement to let users know that the stack is empty.
+
+5. In pop() just before the return statement, reduce the size of the stack by 1.
 """
 
 class Stack:
-  def __init__(self):
+  def __init__(self, limit=1000):
     self.top_item = None
-
-  def peek(self):
-    return self.top_item.get_value()
+    self.limit = limit
+    self.size = 0
 
   def push(self, value):
     item = Node(value)
@@ -39,9 +50,17 @@ class Stack:
     self.top_item = item
 
   def pop(self):
-    item_to_remove = self.top_item
-    self.top_item = item_to_remove
-    return item_to_remove.value
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      # Decrement the stack size here:
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("This stack is totally empty.")
 
   def peek(self):
-    return self.top_item.get_value()
+    if self.size > 0:
+      return self.top_item.get_value()
+    else:
+      print("Nothing to see here!")
