0e6a94eacf7b4b745486fe4f24b7e6096da0d7a1
Kacper <kacwal@poczta.fm>
2019-10-30 19:36:22 +0100

    made input

--- tykurwo/main.py
+++ tykurwo/main.py
@@ -1,6 +1,14 @@
 """its still a implementing stacks"""
 from stack import Stack
 
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
 
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
 
-
+print("\nLet's play Towers of Hanoi!!")
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
