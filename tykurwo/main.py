22f7818714f524101f4fcf6cde874c19f5e4f4c1
Kacper <kacwal@poczta.fm>
2019-10-30 19:47:53 +0100

    made some loops and optimal num of moves

--- tykurwo/main.py
+++ tykurwo/main.py
@@ -12,3 +12,14 @@ stacks.append(right_stack)
 
 print("\nLet's play Towers of Hanoi!!")
 num_disks = int(input("\nHow many disks do you want to play with?\n"))
+
+while num_disks < 3:
+    num_disks = int(input("Enter a number greater than or equal to 3\n"))
+
+for i in range(num_disks, 0, -1):
+    left_stack.push(num_disks)
+
+num_optimal_moves = (2**num_disks) - 1
+
+print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
+
