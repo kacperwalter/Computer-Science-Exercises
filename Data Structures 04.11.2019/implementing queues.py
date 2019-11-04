0ae009e6566284b945d4886e862af5f5082a918c
Kacper <kacwal@poczta.fm>
2019-11-04 20:00:50 +0100

    made exception if queue was empty before adding an element

--- Data Structures 04.11.2019/implementing queues.py
+++ Data Structures 04.11.2019/implementing queues.py
@@ -12,22 +12,42 @@ class Node:
   def get_value(self):
     return self.value
 
+"""
+There are three scenarios that we are concerned with when adding a node to the queue:
+
+1. The queue is empty, so the node we’re adding is both the head and tail of the queue
+2. The queue has at least one other node, so the added node becomes the new tail
+3. The queue is full, so the node will not get added because we don’t want queue “overflow”
+"""
 
 class Queue:
-  # Add max_size and size properties within __init__():
   def __init__(self, max_size=None):
-    self.max_size = max_size
     self.head = None
     self.tail = None
+    self.max_size = max_size
     self.size = 0
 
+  # Add your enqueue method below:
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
+    else:
+      print("Sorry, no more room!")
+
   def peek(self):
-    if self.get_size() == 0:
+    if self.is_empty():
       print("Nothing to see here!")
     else:
       return self.head.get_value()
 
-  # Define get_size() and has_space() below:
   def get_size(self):
     return self.size
 
@@ -38,7 +58,4 @@ class Queue:
       return self.max_size > self.get_size()
 
   def is_empty(self):
-    if self.size == 0:
-      return True
-    else:
-      return False
+    return self.size == 0
