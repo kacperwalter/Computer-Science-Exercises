96372a0a8613ae2145672e33dab787608df84ab7
kacperwalter <kacwal@poczta.fm>
2019-12-07 15:37:38 +0100

    linked lists

--- Python Exercies 22.10.2019/linked nodes python implementation.py
+++ Python Exercies 22.10.2019/linked nodes python implementation.py
@@ -99,7 +99,8 @@ class LinkedList:
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
-
+    
+    # traversing through linked list to make strings from classes
     def stringify_list(self):
         string_list = ""
         current_node = self.get_head_node()
@@ -108,7 +109,8 @@ class LinkedList:
                 string_list += str(current_node.get_value()) + "\n"
                 current_node = current_node.get_next_node()
         return string_list
-
+    
+    # removing node from list
     def remove_node(self, value_to_remove):
         current_node = self.get_head_node()
         if current_node.get_value() == value_to_remove:
@@ -121,4 +123,13 @@ class LinkedList:
                     current_node = None
                 else:
                     current_node = next_node
+
+
+
+
+
+
+
+
+
 
